fcb9d4fa54f98913870d62d9e801aba49e8ebcca
jinho kim <jinho.kim@cloudmt.co.kr>
2024-03-09 17:11:13 +0900

    feat(SQL): add sqlite3

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 
--- 05-onwards-foodies-starting-project/components/meals/meal-item.js
+++ 05-onwards-foodies-starting-project/components/meals/meal-item.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-
 import classes from "./meal-item.module.css";
 
 export default function MealItem({ title, slug, image, summary, creator }) {
