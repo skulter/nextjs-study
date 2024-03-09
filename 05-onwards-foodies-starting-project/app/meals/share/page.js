39d3387109a1ea0692b01e67787e772983e14b4e
jinho kim <jinho.kim@cloudmt.co.kr>
2024-03-09 22:57:29 +0900

    feat(form): 이미지 버튼 추가 및 미리보기 추가

--- 05-onwards-foodies-starting-project/app/meals/share/page.js
+++ 05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -1,3 +1,4 @@
+import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
@@ -50,6 +51,7 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     IMAGE PICKER
+                    <ImagePicker />
                     <p className={classes.actions}>
                         <button type="submit">Share Meal</button>
                     </p>
