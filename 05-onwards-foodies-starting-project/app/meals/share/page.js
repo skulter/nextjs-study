38246c2011fe1b2c314d562be986fd56e7822222
jinho kim <jinho.kim@cloudmt.co.kr>
2024-03-10 01:19:41 +0900

    feat(meals): form 제출 시 버튼 텍스트 변경
    - useFormStatus() 훅 사용

--- 05-onwards-foodies-starting-project/app/meals/share/page.js
+++ 05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -1,21 +1,9 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
-    async function shareMeal(formData) {
-        "use server";
-        const meal = {
-            title: formData.get("title"),
-            summary: formData.get("summary"),
-            instructions: formData.get("instructions"),
-            image: formData.get("image"),
-            creator: formData.get("name"),
-            creator_email: formData.get("email"),
-        };
-
-        console.log(meal);
-    }
-
     return (
         <>
             <header className={classes.header}>
@@ -67,7 +55,8 @@ export default function ShareMealPage() {
                     IMAGE PICKER
                     <ImagePicker label="your image" name="image" />
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit />
+                        {/* <button type="submit">Share Meal</button> */}
                     </p>
                 </form>
             </main>
