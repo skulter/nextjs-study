cc785f09d8e1904a3c9eb6bff2529e044360081a
jinho kim <jinho.kim@cloudmt.co.kr>
2024-03-09 18:09:31 +0900

    feat(loading): add Suspense, fallback

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -2,9 +2,15 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import Loading from "./loading-out";
+
+async function Meals() {
+    const meals = await getMeals();
+    return <MealsGrid meals={meals} />;
+}
 
 export default async function MealsPage() {
-    const meals = await getMeals();
     return (
         <>
             <header className={classes.header}>
@@ -21,7 +27,9 @@ export default async function MealsPage() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<Loading />}>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     );
